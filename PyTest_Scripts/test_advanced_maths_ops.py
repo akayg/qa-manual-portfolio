25458889087b283382fc436f28ccef8f8513281d
Shivpreet Saini <155538486+shivsaini01@users.noreply.github.com>
2025-06-23 23:53:44 +0530

    Create test_advanced_maths_ops.py
    Create test_advanced_maths_ops.py

---
+++ PyTest_Scripts/test_advanced_maths_ops.py
@@ -0,0 +1,13 @@
+# test_advanced_maths_ops.py
+
+def test_power():
+    assert 2**3 == 8
+
+def test_modulo():
+    assert 10 % 3 == 1
+
+def test_floor_division():
+    assert 9 // 2 == 4
+
+def test_negative_multiplication():
+    assert -4 * 2 == -8
